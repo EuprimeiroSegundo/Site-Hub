1703195355974f8f791c7327492e2264d02b8f07
EuprimeiroSegundo <gust.notific.pessoal@gmail.com>
2022-03-18 16:39:51 -0300

    Mudança
    .

---
+++ Hub/Hub/ApplicationContext.cs
@@ -0,0 +1,18 @@
+using Hub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hub
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options): base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CadastroUsuario>().HasKey(t => t.Id);
+        }
+
+    }
+}
